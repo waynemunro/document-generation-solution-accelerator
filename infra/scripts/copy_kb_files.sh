c57d877d656a571be7317def03ffcc6f7cdc1ffb
Malory Rose <maloryrose07@outlook.com>
2025-04-02 17:49:23 -0700

    update post deployment scripts

--- infra/scripts/copy_kb_files.sh
+++ infra/scripts/copy_kb_files.sh
@@ -3,8 +3,8 @@
 # Variables
 storageAccount="$1"
 fileSystem="$2"
-baseUrl="$3"
-managedIdentityClientId="$4"
+# baseUrl="$3"
+managedIdentityClientId="$3"
 
 zipFileName1="pdfdata.zip"
 extractedFolder1="pdf"
@@ -15,15 +15,15 @@ zipUrl1=${baseUrl}"infra/data/pdfdata.zip"
 # zipUrl2=${baseUrl}"infra/data/audio_data.zip"
 
 # Create folders if they do not exist
-mkdir -p "/mnt/azscripts/azscriptinput/$extractedFolder1"
+# mkdir -p "/mnt/azscripts/azscriptinput/$extractedFolder1"
 # mkdir -p "/mnt/azscripts/azscriptinput/$extractedFolder2"
 
 # Download the zip file
-curl --output /mnt/azscripts/azscriptinput/"$zipFileName1" "$zipUrl1"
+# curl --output /mnt/azscripts/azscriptinput/"$zipFileName1" "$zipUrl1"
 # curl --output /mnt/azscripts/azscriptinput/"$zipFileName2" "$zipUrl2"
 
 # Extract the zip file
-unzip /mnt/azscripts/azscriptinput/"$zipFileName1" -d /mnt/azscripts/azscriptinput/"$extractedFolder1"
+unzip infra/data/"$zipFileName1" -d infra/data/"$extractedFolder1"
 # unzip /mnt/azscripts/azscriptinput/"$zipFileName2" -d /mnt/azscripts/azscriptinput/"$extractedFolder2"
 
 echo "Script Started"
@@ -31,5 +31,5 @@ echo "Script Started"
 # Authenticate with Azure using managed identity
 az login --identity --client-id ${managedIdentityClientId}
 # Using az storage blob upload-batch to upload files with managed identity authentication, as the az storage fs directory upload command is not working with managed identity authentication.
-az storage blob upload-batch --account-name "$storageAccount" --destination data/"$extractedFolder1" --source /mnt/azscripts/azscriptinput/"$extractedFolder1" --auth-mode login --pattern '*' --overwrite
+az storage blob upload-batch --account-name "$storageAccount" --destination data/"$extractedFolder1" --source infra/data/"$extractedFolder1" --auth-mode login --pattern '*' --overwrite
 # az storage blob upload-batch --account-name "$storageAccount" --destination data/"$extractedFolder2" --source /mnt/azscripts/azscriptinput/"$extractedFolder2" --auth-mode login --pattern '*' --overwrite
--- infra/scripts/run_create_index_scripts.sh
+++ infra/scripts/run_create_index_scripts.sh
@@ -2,19 +2,28 @@
 echo "started the script"
 
 # Variables
-baseUrl="$1"
-keyvaultName="$2"
-managedIdentityClientId="$3"
-requirementFile="requirements.txt"
-requirementFileUrl=${baseUrl}"infra/scripts/index_scripts/requirements.txt"
+# baseUrl="$1"
+keyvaultName="$1"
+managedIdentityClientId="$2"
+# requirementFile="infra/scripts/index_scripts/requirements.txt"
+# requirementFileUrl=${baseUrl}"infra/scripts/index_scripts/requirements.txt"
 
 echo "Script Started"
 
-# Download the create_index and create table python files
-curl --output "01_create_search_index.py" ${baseUrl}"infra/scripts/index_scripts/01_create_search_index.py"
-curl --output "02_process_data.py" ${baseUrl}"infra/scripts/index_scripts/02_process_data.py"
+echo "Getting signed in user id"
+signed_user_id=$(az ad signed-in-user show --query id -o tsv)
 
+# # Download the create_index and create table python files
+# curl --output "01_create_search_index.py" ${baseUrl}"infra/scripts/index_scripts/01_create_search_index.py"
+# curl --output "02_process_data.py" ${baseUrl}"infra/scripts/index_scripts/02_process_data.py"
 
+# Define the scope for the Key Vault (replace with your Key Vault resource ID)
+echo "Getting key vault resource id"
+key_vault_resource_id=$(az keyvault show --name $keyvaultName --query id --output tsv)
+
+# Assign the Key Vault Administrator role to the user
+echo "Assigning the Key Vault Administrator role to the user."
+az role assignment create --assignee $signed_user_id --role "Key Vault Administrator" --scope $key_vault_resource_id
 
 # RUN apt-get update
 # RUN apt-get install python3 python3-dev g++ unixodbc-dev unixodbc libpq-dev
@@ -24,17 +33,17 @@ curl --output "02_process_data.py" ${baseUrl}"infra/scripts/index_scripts/02_pro
 # pip install pyodbc
 
 # Download the requirement file
-curl --output "$requirementFile" "$requirementFileUrl"
+# curl --output "$requirementFile" "$requirementFileUrl"
 
-echo "Download completed"
+# echo "Download completed"
 
 #Replace key vault name 
-sed -i "s/kv_to-be-replaced/${keyvaultName}/g" "01_create_search_index.py"
-sed -i "s/mici_to-be-replaced/${managedIdentityClientId}/g" "01_create_search_index.py"
-sed -i "s/kv_to-be-replaced/${keyvaultName}/g" "02_process_data.py"
-sed -i "s/mici_to-be-replaced/${managedIdentityClientId}/g" "02_process_data.py"
+sed -i "s/kv_to-be-replaced/${keyvaultName}/g" "infra/scripts/index_scripts/01_create_search_index.py"
+sed -i "s/mici_to-be-replaced/${managedIdentityClientId}/g" "infra/scripts/index_scripts/01_create_search_index.py"
+sed -i "s/kv_to-be-replaced/${keyvaultName}/g" "infra/scripts/index_scripts/02_process_data.py"
+sed -i "s/mici_to-be-replaced/${managedIdentityClientId}/g" "infra/scripts/index_scripts/02_process_data.py"
 
-pip install -r requirements.txt
+pip install -r infra/scripts/index_scripts/requirements.txt
 
-python 01_create_search_index.py
-python 02_process_data.py
+python infra/scripts/index_scripts/01_create_search_index.py
+python infra/scripts/index_scripts/02_process_data.py
